380dcc88a5281bc4de9ffd0970beaf49e47fb8bb
sushant-sg9 <sushantghadge2016@gmail.com>
2022-09-15 00:23:56 +0530

    assignment6

---
+++ src/models/Blog.js
@@ -0,0 +1,13 @@
+const mongooose = require('mongoose');
+
+const blogSchema = new mongooose.Schema({
+    // Your code goes here
+    topic:String,
+    description:String,
+    posted_at:String,
+    posted_by:String,
+})
+
+const Blog = mongooose.model('blogs', blogSchema);
+
+module.exports = Blog;
